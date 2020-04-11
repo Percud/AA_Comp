1056a3b57ac175a5fd55fb5666bba561c5dfefbd
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-12 00:30:53 +0200

    Update Get_universal_singlecopy_orthogroups.R

--- Get_universal_singlecopy_orthogroups.R
+++ Get_universal_singlecopy_orthogroups.R
@@ -8,17 +8,17 @@ library(Biostrings)
 #that are present in >90% of species and are single copy in >90% of species 
 OGs<-fromJSON(file='https://www.orthodb.org//search?level=7742&universal=0.9&singlecopy=0.9&limit=5000')
 
-for(OG in OGs$data[1:10]){
+for(OG in OGs$data[1:3]){
   #Sauropsida - taxid:8457   
-  url=paste("http://www.orthodb.org/fasta?id=",OG,"&species=","8457",sep="")
+  url<-paste("http://www.orthodb.org/fasta?id=",OG,"&species=","8457",sep="")
   seq<-readAAStringSet(open_input_files(url))
   writeXStringSet(seq, "Sauropsida.fa", append=TRUE)
   #Mammalia - taxid:40674 
-  url=paste("http://www.orthodb.org/fasta?id=",OG,"&species=","40674",sep="")
+  url<-paste("http://www.orthodb.org/fasta?id=",OG,"&species=","40674",sep="")
   seq<-readAAStringSet(open_input_files(url))
   writeXStringSet(seq, "Mammalia.fa", append=TRUE)
   #Actinopterygii - taxid:7898 
-  url=paste("http://www.orthodb.org/fasta?id=",OG,"&species=","7898",sep="")
+  url<-paste("http://www.orthodb.org/fasta?id=",OG,"&species=","7898",sep="")
   seq<-readAAStringSet(open_input_files(url))
   writeXStringSet(seq, "Fishes.fa", append=TRUE) 
 }
