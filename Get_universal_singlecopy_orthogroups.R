97b8278ede0b9add9ec31f3d2393c87402ecf788
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-12 00:58:44 +0200

    Update Get_universal_singlecopy_orthogroups.R

--- Get_universal_singlecopy_orthogroups.R
+++ Get_universal_singlecopy_orthogroups.R
@@ -17,7 +17,7 @@ for(OG in OGs$data[1:3]){
   writeXStringSet(seq, "Sauropsida.fa", append=TRUE)
   #Mammalia - taxid:40674 
   url2<-paste("http://www.orthodb.org/fasta?id=",OG,"&species=","40674",sep="")
-  seq<-readAAStringSet(open_input_files(url))
+  seq<-readAAStringSet(open_input_files(url2))
   writeXStringSet(seq, "Mammalia.fa", append=TRUE)
   cat(url)
   #Actinopterygii - taxid:7898 
