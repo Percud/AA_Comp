82537703ff701705e35d46e34617ec891e98bb2a
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-15 00:26:24 +0200

    Update Plot_Orthogroup.R

--- Utilities/Plot_Orthogroup.R
+++ Utilities/Plot_Orthogroup.R
@@ -27,7 +27,7 @@ if (length(aa)>=2){
   y=df[[aa]]
   qplot(x,y)+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=c("Actinopterygii","Sauropsida","Mammalia"))+ylab(paste("Number of",aa,"in the sequence"))
 } else {
-  plt_array<-apply(aa,function(i){qplot(x,df[[i]])+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=c("Actinopterygii","Sauropsida","Mammalia"))+ylab(paste("Number of",i,"in the sequence"))})
+  plt_array<-sapply(aa,function(i){qplot(x,df[[i]])+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=c("Actinopterygii","Sauropsida","Mammalia"))+ylab(paste("Number of",i,"in the sequence"))})
   grid.arrange(plt_array, ncol=5, top=textGrob("AA", gp=gpar(fontsize=12, font = 2)))
 }
 
