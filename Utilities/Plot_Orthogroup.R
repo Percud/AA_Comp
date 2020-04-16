2b04b728c8f534fc62c0e2de370a7b30e8cf6190
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-16 18:28:37 +0200

    Update Plot_Orthogroup.R

--- Utilities/Plot_Orthogroup.R
+++ Utilities/Plot_Orthogroup.R
@@ -27,12 +27,12 @@ x=df[['Classification']]
 
 pdf(file = "aaPlot.pdf", width = 14, height = 10) # defaults to 7 x 7 inches
 
-g=c("boxplot","violin")
+#geom_violin(trim=FALSE, fill="gray")+geom_boxplot(width=0.1)+theme_classic()
 
 if (length(aa)==1){ #single plot
-  qplot(x,df[[aa]],geom=g)+ylim(0, 100)+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",aa,"in the sequence"))
+  qplot(x,df[[aa]])+geom_violin(trim=FALSE, fill="gray")+geom_boxplot(width=0.1)+theme_classic()+ylim(0, 100)+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",aa,"in the sequence"))
 } else { #multi plot
-  pltList<-lapply(aa,function(i){qplot(x,df[[i]],geom=g)+ylim(0, 100)+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",i,"in the sequence"))})
+  pltList<-lapply(aa,function(i){qplot(x,df[[i]])+geom_violin(trim=FALSE, fill="gray")+geom_boxplot(width=0.1)+theme_classic()+ylim(0, 100)+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",i,"in the sequence"))})
   do.call(grid.arrange, c(pltList, ncol=5))
 }
 
