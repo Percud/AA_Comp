4c62579f10f2f97cdb2728ed27bbff82fa17741c
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-16 10:57:41 +0200

    Update Plot_Orthogroup.R

--- Utilities/Plot_Orthogroup.R
+++ Utilities/Plot_Orthogroup.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(gridExtra)
 library(grid)
 
+Taxa=c("Actinopterygii","Sauropsida","Mammalia")
 aa = c("A","C","D","E","F","G","H","I","K","L","M","N","R","P","Q","S","T","Y","W","V")
 args = commandArgs(trailingOnly=TRUE)
 
@@ -28,9 +29,9 @@ pdf(file = "aaPlot.pdf", width = 14, height = 10) # defaults to 7 x 7 inches
 
 
 if (length(aa)==1){ #single plot
-  qplot(x,df[[aa]])+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=c("Actinopterygii","Sauropsida","Mammalia"))+ylab(paste("Number of",aa,"in the sequence"))
+  qplot(x,df[[aa]])+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",aa,"in the sequence"))
 } else { #multi plot
-  pltList<-lapply(aa,function(i){qplot(x,df[[i]])+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=c("Actinopterygii","Sauropsida","Mammalia"))+ylab(paste("Number of",i,"in the sequence"))})
+  pltList<-lapply(aa,function(i){qplot(x,df[[i]])+geom_boxplot()+geom_jitter()+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",i,"in the sequence"))})
   do.call(grid.arrange, c(pltList, ncol=5))
 }
 
