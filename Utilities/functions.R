0b1cd48d468e079214bd2ffcd149c05877a127b1
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-20 14:49:10 +0200

    Update functions.R

--- Utilities/functions.R
+++ Utilities/functions.R
@@ -2,9 +2,9 @@
 g_median = function(T, seq_id) {
 
   gene<-T[which(T$seq_id == seq_id),]
-  #vector (drop=T) of sequence widths for the same Classification and pub_og_id as seq_id
-  widths<-T[which(T$Classification == gene$Classification & T$pub_og_id == gene$pub_og_id), "width", drop=T]
+  #data.frame of sequence widths for the same Classification and pub_og_id as seq_id
+  widths<-T[which(T$Classification == gene$Classification & T$pub_og_id == gene$pub_og_id), "width"]
   
-return(median(widths))
+return(median(widths[['withd']]))
 }
   
