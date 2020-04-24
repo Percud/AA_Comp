9a2fd5403bb34e976efbe9368122a1c920731b1f
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-04-25 00:11:20 +0200

    Update functions.R

--- Utilities/functions.R
+++ Utilities/functions.R
@@ -11,8 +11,8 @@ AA_Comp_10$Seq.pass<-with(AA_Comp_10, width>=g_median-g_median*0.15 & width<=g_m
 AA_Comp_10$Group.pass<-ave(AA_Comp_10$Seq.pass,AA_Comp_10$pub_og_id,FUN=function (x) sum(x)/length(x))
 
 #pairwise test
-function format.t-test(x, pairwise){
-  p<-x[['pvalue']]
+format_ttest <- function (x, pairwise){
+  p=x[['p.value]]
   m<-apply(combn(Tax$name,2),2, function(x) p[x[1],x[2]])
   names(m)<-apply(combn(Tax$name,2),2, function(x) paste(x[1],x[2],'pvalue',sep="."))
   return(as.data.frame(t(m)))
