0f989c0be0adc99d032efc3274467934b6deeb02
JasmineRagazzini <63784384+JasmineRagazzini@users.noreply.github.com>
2020-05-22 17:08:51 +0200

    Update Boxplot.R

--- Utilities/Boxplot.R
+++ Utilities/Boxplot.R
@@ -10,10 +10,9 @@ library(grid)
 Taxa=c("Actinopterygii","Sauropsida","Mammalia")
 aa = c("A","C","D","E","F","G","H","I","K","L","M","N","R","P","Q","S","T","Y","W","V")
 args = commandArgs(trailingOnly=TRUE)
+args = "100129at7742"
 
-
-fname="AA_Comp_10.csv"
-df<-read.csv(fname,  header=TRUE)
+df<-read.csv("AA_Comp.csv",  header=TRUE)
 
 og<-args[1]
 if (length(args)==2){
@@ -30,25 +29,21 @@ df[,aa]<-df[,aa]/df$width*100 #percentage
 
 x=df[['Classification']]
 
-pdf(file = "aaPlot.pdf", width = 15, height = 10) # defaults to 7 x 7 inches
+pdf(file = paste0(args,"Boxplot.pdf"), width = 15, height = 10) # defaults to 7 x 7 inches
 
 #geom_violin(fill="gray")+stat_summary(fun.data="mean_cl_boot", colour="red", size=1)+theme_classic()
 
 
 if (length(aa)==1){ #single plot
-  qplot(x,df[[aa]],geom="blank")+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",aa,"in the sequence"))+
+  qplot(x,df[[aa]],geom="blank")+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Percentage of",aa))+
   geom_boxplot(outlier.shape = NA) + geom_jitter(width = 0.2)
 } else { #multi plot
-  pltList<-lapply(aa,function(i){qplot(x,df[[i]],geom="blank")+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Number of",i,"in the sequence"))+
+  pltList<-lapply(aa,function(i){qplot(x,df[[i]],geom="blank")+scale_x_discrete(name="",limits=Taxa)+ylab(paste("Percentage of",i))+
   #geom_boxplot(outlier.shape = NA) + geom_jitter(width = 0.2)
   geom_boxplot(outlier.shape = NA)+ ylim(0,15)})
   do.call(grid.arrange, c(pltList, ncol=5))
 }
 warnings()
 dev.off()
-
-
-
-
 
 
