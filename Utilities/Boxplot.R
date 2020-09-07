c86bc0002061ced7ae139e0e8c274046e6de07b3
JasmineRagazzini <63784384+JasmineRagazzini@users.noreply.github.com>
2020-09-07 15:05:52 +0200

    Update Boxplot.R

--- Utilities/Boxplot.R
+++ Utilities/Boxplot.R
@@ -11,14 +11,14 @@ Taxa=c("Actinopterygii","Sauropsida","Mammalia")
 aa = c("A","C","D","E","F","G","H","I","K","L","M","N","R","P","Q","S","T","Y","W","V")
 args = commandArgs(trailingOnly=TRUE)
 
-df<-read.csv("AA_Comp.csv",  header=TRUE)
+df<-read.csv(args[1],  header=TRUE)
 
-og<-args[1]
-if (length(args)==2){
-  aa<-args[2]
+og<-args[2]
+if (length(args)==3){
+  aa<-args[3]
 }
 
-if (args[1]!="all"){
+if (args[2]!="all"){
   df <- filter(df, pub_og_id==og)
 }
 
@@ -28,7 +28,7 @@ df[,aa]<-df[,aa]/df$width*100 #percentage
 
 x=df[['Classification']]
 
-pdf(file = paste0(args,"Boxplot.pdf"), width = 15, height = 10) # defaults to 7 x 7 inches
+pdf(file = paste0(args[2],"Boxplot.pdf"), width = 15, height = 10) # defaults to 7 x 7 inches
 
 #geom_violin(fill="gray")+stat_summary(fun.data="mean_cl_boot", colour="red", size=1)+theme_classic()
 
