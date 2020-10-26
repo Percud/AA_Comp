07846c4024bd826454263643b900aed7c24efa0f
JasmineRagazzini <63784384+JasmineRagazzini@users.noreply.github.com>
2020-10-26 16:48:28 +0100

    Update Barplot.R

--- Utilities/Barplot.R
+++ Utilities/Barplot.R
@@ -8,6 +8,9 @@ pair_matrix=combn(Tax$name,2)
 args=commandArgs(trailingOnly=TRUE)
 
 Res<-read.csv(args[1], header=TRUE)
+pvalue.max<-args[2]
+FC.min<-args[3]
+FC.max<-args[4]
 
 for(i in 1:ncol(pair_matrix)){
   m<-c(pair_matrix[1,i],pair_matrix[2,i])
@@ -15,15 +18,15 @@ for(i in 1:ncol(pair_matrix)){
   FC<-(paste0(pair_matrix[1,i],".",pair_matrix[2,i],".fold_change"))
   
   df<-Res %>% filter(.[m[1]]>=10 | .[m[2]]>=10) %>% 
-    filter(.[pv]<=args[2])   %>% 
+    filter(.[pv]<=pvalue.max)   %>% 
     group_by(AA) %>% 
-    group_modify (~ summarize(.x,up=sum(.[FC]>=args[3]), down=-sum(.[FC]<=args[4]))) %>%
+    group_modify (~ summarize(.x,up=sum(.[FC]>=FC.min), down=-sum(.[FC]<=FC.max))) %>%
     arrange(desc(up-abs(down)))
   
   #reshape
   df<-reshape(as.data.frame(df), v.names="count", timevar="var",times = c("up","down"), varying= c("up","down"), direction="long")
   
-  print(ggplot(df, aes(x=reorder(AA,id) ,y=count,fill=var)) + geom_bar(stat="identity") + ggtitle(paste(pv,"<=",args[2],"\n",args[4],"<=",FC,">=",args[3])) +
+  print(ggplot(df, aes(x=reorder(AA,id) ,y=count,fill=var)) + geom_bar(stat="identity") + ggtitle(paste(pv,"<=",pvalue.max,"\n",FC.max,"<=",FC,">=",FC.min)) +
           xlab("AA") + ylab("Orthogroups count"))
   
   readline(prompt=paste(i,"- Press [enter] to continue"))
